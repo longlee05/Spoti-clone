53482807988f5e8bcd6a00e0cc75d0c0798e5f68
Xenon <proday10x@gmail.com>
2025-06-25 16:53:00 +0700

    add displayalbum

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
-import DIsplayAlbum from './DIsplayAlbum'
+import DisplayAlbum from './DIsplayAlbum'
+
 
 const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
             <Route path='/' element={<DisplayHome/>} />
-            <Route path='/album/:id' element={<DIsplayAlbum/>} />
+            <Route path='/album/:id' element={<DisplayAlbum/>} />
         </Routes>
     </div>
   )
